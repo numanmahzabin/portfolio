48bdd2bd491c47458c7342c22fdd13500c93f515
numanmahzabin <numanmahzabin1029@gmail.com>
2024-08-02 20:29:06 +0600

    revision

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       fontFamily : {
-        // "ovo" : ["Ovo"],
-        // "outfit" : ["Outfit"]
+        "ovo" : ["Ovo"],
+        "outfit" : ["Outfit"]
       },
       colors:{
         "blue" : "#110020"
